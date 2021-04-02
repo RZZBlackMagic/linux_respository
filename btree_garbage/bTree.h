b02960c6631e43aaafb31b065a83b45481f39bf1
RZZBlackMagic <1403741992@qq.com>
2021-04-02 22:20:17 +0800

    btree

--- btree_garbage/bTree.h
+++ btree_garbage/bTree.h
@@ -1,26 +1,30 @@
 #include<iostream>
+#include<string.h>
 using namespace std;
-//const int M = 4;//节点的最大数量
+const int M = 4;//节点的最大数量
 template<typename T>
 class b_tree_node{
 public:
-	int degree;//度：此节点的子节点个数
-	int num;//key的个数，奇数
+	int degree;//度:所有节点的最大字节点数
+	int num;//key的个数
 	T* keys;
 	b_tree_node** childs;
 	b_tree_node* parent;
 	bool leaf;//是否为叶节点  false:不是   true:是
 	b_tree_node(){};
-	b_tree_node(int degree,int leaf){
+	b_tree_node(int degree,bool leaf1){
 		this->degree = degree;
 		keys = new T[degree-1];//最多有degree-1个key
 		childs = new b_tree_node*[degree];//最多有degree个子节点
-		leaf = leaf;
-		num = sizeof(keys)/sizeof(T);
+		memset(keys,-1,sizeof(keys));
+		memset(childs,NULL,sizeof(childs));
+		parent = NULL;
+		this->leaf = leaf1;
+		num = 0;
 	};
 	~b_tree_node(){
-		keys = NULL;
-		childs = NULL;
+		//keys = NULL;
+		//childs = NULL;
 		num = -1;
 		degree = -1;
 		leaf = false;
@@ -29,15 +33,14 @@ public:
 
 template<typename T>
 class b_tree{
-	static int M ;
 	b_tree_node<T>* root;
 	b_tree_node<T>* Nil;
 	//节点分裂,返回1：分裂成功，返回0：分裂失败
-	static b_tree_node<T>* split_node(b_tree_node<T>*& root,b_tree_node<T>* node,int flag);
+	static b_tree_node<T>* split_node(b_tree_node<T>**& root,b_tree_node<T>* node,int flag);
 	//寻找插入位置
-	static b_tree_node<T>* find_insert_node(b_tree_node<T>*& cur,b_tree_node<T>* curr,T x);
+	static b_tree_node<T>* find_insert_node(b_tree_node<T>**& cur,b_tree_node<T>* curr,T x);
 	//判断当前节点是否存满。满了返回1，未满就返回0
-	static inline int check_node_state(b_tree_node<T>* cur);
+	static inline int check_node_state(b_tree_node<T>* cur,int i);
 public:
 	b_tree();
 	//通过key来查找
@@ -49,12 +52,13 @@ public:
 	//打印
 	static void print_tree(b_tree_node<T>* node);
 	//得到root
-	b_tree_node<T>* getRoot();
+	b_tree_node<T>** getRoot();
 };
 //得到root
 template<typename T>
-b_tree_node<T>* b_tree<T>::getRoot(){
-	return root;
+b_tree_node<T>** b_tree<T>::getRoot(){
+	//cout<<"this->root"<<&(this->root)<<endl;
+	return &(this->root);
 }
 //b树构造函数
 template<typename T>
@@ -63,9 +67,16 @@ b_tree<T>::b_tree(){
 };
 //判断当前节点的字节点是否存满
 template<typename T>
-inline int b_tree<T>::check_node_state(b_tree_node<T>* cur){
+inline int b_tree<T>::check_node_state(b_tree_node<T>* cur,int m){
 	//M阶b树，最多有M-1个key
-	if(cur->degree==M){
+	int n = 0;
+	for(int i=0;i<cur->num;i++){
+		if(cur->keys[i] == -1){
+			break;
+		}
+		n++;
+	}
+	if(n == m){
 //		cout<<"fen"<<endl;
 		return 1;
 	}
@@ -76,173 +87,180 @@ inline int b_tree<T>::check_node_state(b_tree_node<T>* cur){
 //flag=0：cur指向分裂后的左边
 //返回值：指向分裂后的插入位置
 template<typename T>
-b_tree_node<T>* b_tree<T>::split_node(b_tree_node<T>*& root,b_tree_node<T>* node,int flag){
-	if(node==NULL)
-		return NULL;
-	//取此节点的中间节点（n/2向上取整）作为新节点，其他关键字形成两个字节点作为新节点的左右孩子,新节点加到父节点里面，递归处理副节点知道root
-	int middle_key = node->num/2;//向上取整(向上取整要加1,但是下表从0开始再减1)
-	b_tree_node<T>* middle_node = (b_tree_node<T>*)malloc(sizeof(b_tree_node<T>));
-	b_tree_node<T>* left_node = (b_tree_node<T>*)malloc(sizeof(b_tree_node<T>));
-	b_tree_node<T>* right_node = (b_tree_node<T>*)malloc(sizeof(b_tree_node<T>));
-	//复制左右节点
-	left_node->num = middle_key;
-	left_node->degree = node->degree/2;
-	left_node->leaf = node->leaf;
-	left_node->parent = middle_node;
-	left_node->keys = (T*)malloc(sizeof(T)*left_node->num);
-	left_node->childs = (b_tree_node<T>**)malloc(sizeof(b_tree_node<T>*)*left_node->degree);
-	for(int i=0;i<middle_key;i++){
+b_tree_node<T>* b_tree<T>::split_node(b_tree_node<T>**& root,b_tree_node<T>* node,int flag){
+//	cout<<"cccc"<<root<<endl;
+//	cout<<node->keys[node->num/2]<<endl;
+	//找节点最中间的数据提到父节点中
+	int middle_index = node->num/2;
+//	cout<<node->keys[middle_index]<<endl;
+	//处理左字节点
+	b_tree_node<T>* left_node = new b_tree_node<T>(4,node->leaf);
+	for(int i=0;i<middle_index;i++){
 		left_node->keys[i] = node->keys[i];
+		left_node->num++;
 		left_node->childs[i] = node->childs[i];
 	}
-	left_node->childs[middle_key] = node->childs[middle_key];
-
-	right_node->num = middle_key;
-	right_node->degree = node->degree/2;
-	right_node->leaf = node->leaf;
-	right_node->parent = middle_node;
-	right_node->keys = (T*)malloc(sizeof(T)*right_node->num);
-	right_node->childs = (b_tree_node<T>**)malloc(sizeof(b_tree_node<T>*)*right_node->degree);
-	for(int i=middle_key+1,j=0;i<node->num;i++,j++){
-		right_node->keys[j++] = node->keys[i];
-		right_node->childs[j++] = node->childs[i];
+	left_node->childs[middle_index] = node->childs[middle_index];
+//	cout<<"left_node:"<<left_node->num<<endl;	
+	//处理右字节点
+	b_tree_node<T>* right_node = new b_tree_node<T>(4,node->leaf);
+	for(int i=middle_index+1;i<node->num;i++){
+		right_node->keys[i-middle_index-1] = node->keys[i];
+		right_node->num++;
+		right_node->childs[i-middle_index-1] = node->childs[i];
 	}
-	right_node->childs[right_node->degree-1] = node->childs[node->num];
-	//从上到下检查，所以此节点的父节点都检查过了，一定是未满，直接插入父节点
+	right_node->childs[right_node->num] = node->childs[node->degree];
+//	cout<<"right_node:"<<right_node->num<<endl;
+	//处理父节点
+	b_tree_node<T>* new_parent = new b_tree_node<T>(4,false);
 	b_tree_node<T>* parent = node->parent;
 	if(parent==NULL){
-		//node是跟节点
-//		cout<<"d"<<endl;
-		middle_node->num = 1;
-		middle_node->degree = 2;
-		middle_node->leaf = false;
-		middle_node->parent = NULL;
-		middle_node->keys = (T*)malloc(sizeof(T));
-		middle_node->childs = (b_tree_node<T>**)malloc(sizeof(b_tree_node<T>*)*2);
-		middle_node->keys[0] = node->keys[middle_key];
-		middle_node->childs[0] = left_node;
-		middle_node->childs[1] = right_node;
-		root = middle_node;
-		free(node);
-		if(flag == 0){
-			return left_node;	
-		}else{
-			return right_node;
-		}
-	}
-	//寻找加入位置
-	int insert_index = 0;
-	for(insert_index=0;insert_index<parent->num;insert_index++){
-		if(node->keys[middle_key]>parent->keys[insert_index])
-			continue;
-		break;
-	}
-	//cout<<"insert_index:"<<insert_index<<endl;
-	//在parent的insert_index插入
-	b_tree_node<T>* new_parent = (b_tree_node<T>*)malloc(sizeof(b_tree_node<T>));
-	new_parent->num = parent->num+1;
-	new_parent->degree = parent->degree+1;
-	new_parent->leaf = parent->leaf;
-	new_parent->parent = parent->parent;
-	new_parent->keys = (T*)malloc(sizeof(T)*new_parent->num);
-	new_parent->childs = (b_tree_node<T>**)malloc(sizeof(b_tree_node<T>*)*new_parent->degree);
-	bool f = false;
-	for(int i=0;i<parent->num;i++){
-		if(insert_index==i){
-			new_parent->keys[i] = node->keys[middle_key];
-			new_parent->childs[i] = left_node;
-			new_parent->childs[i+1] = right_node;
-			continue;
-		}
-		if(insert_index>i){
-			new_parent->keys[i] = parent->keys[i-1];
-			if(f==false){
-				f = true;
+		//node就是跟节点
+	//	cout<<"aa"<<endl;
+		*root = new_parent;
+		new_parent->keys[0] = node->keys[middle_index];
+		new_parent->childs[0] = left_node;
+		new_parent->childs[1] = right_node;
+		new_parent->num++;
+	}else{
+		bool flag1 = true;
+		//处理keys
+		for(int i=0;i<parent->num;i++){
+			if(parent->keys[i] < node->keys[middle_index]){
+//				cout<<"aaaaa"<<node->keys[middle_index]<<endl;
+				new_parent->keys[i] = parent->keys[i];
+				new_parent->num++;
 				continue;
 			}
-			new_parent->childs[i] = parent->childs[i-1];
+			if(flag1){
+				new_parent->keys[i] = node->keys[middle_index];
+				new_parent->num++;
+				flag1 = false;
+				continue;
+			}
+			new_parent->keys[i] = parent->keys[i-1];
+			new_parent->num++;
 		}
-		if(insert_index<i){
-			new_parent->childs[i] = parent->childs[i];
-			new_parent->keys[i] = parent->keys[i];
+		if(flag1){
+//			cout<<"aaaaaaaaaaaaaaaaaa"<<node->num<<endl;
+			//在最后进行添加
+			new_parent->keys[parent->num] = node->keys[middle_index];
+			new_parent->num++;
+		}else{
+			new_parent->keys[parent->num] = parent->keys[node->num-1];
+			new_parent->num++;
 		}
-	}
-	//key比child少一个，将最后一个child复制进来
-	if(insert_index==parent->num){
-		new_parent->keys[insert_index] = node->keys[middle_key];
-		new_parent->childs[insert_index] = left_node;  
-		new_parent->childs[insert_index+1] = right_node;
-	}else{
-		new_parent->keys[new_parent->num-1] = parent->keys[parent->num-1];
-		new_parent->childs[parent->num] = parent->childs[parent->degree-2];
-		new_parent->childs[parent->num+1] = parent->childs[parent->degree-1];
-	}
-	//最后修改new_parent的父指针的该子指针
-	if(parent->parent!=NULL){
-		if(parent->parent->parent == NULL){
-			//parent的父节点是root
-			for(int i=0;i<parent->parent->degree;i++){
+//		cout<<new_parent->keys[0]<<endl;
+//              cout<<new_parent->keys[1]<<endl;
+		//处理childs
+		for(int i=0;i<parent->num+1;i++){
+			if(new_parent->keys[i] < node->keys[middle_index]){
+				new_parent->childs[i] = parent->childs[i];
+				continue;
+			}
+			if(new_parent->keys[i] == node->keys[middle_index]){
+				new_parent->childs[i] = left_node;
+				new_parent->childs[i+1] = right_node;
+				continue;
+			}
+			new_parent->childs[i+1] = parent->childs[i]; 
+		}
+		new_parent->parent = parent->parent;
+		if(parent->parent!=NULL){
+			for(int i=0;i<parent->parent->num;i++){
 				if(parent->parent->childs[i] == parent){
+					cout<<"********"<<endl;
 					parent->parent->childs[i] = new_parent;
-				
+					break;
 				}
 			}
+		}else{
+//			cout<<"aaa"<<endl;
+//			cout<<*root<<endl;
+			*root = new_parent;
+//			cout<<*root<<endl;
+//			cout<<(*root)->num<<endl;
+//			cout<<(*root)->keys[0]<<endl;
+//			cout<<(*root)->keys[1]<<endl;
+//			cout<<node->keys[middle_index]<<endl;
 		}
-	}else{
-//		cout<<"aa"<<endl;
-		root = new_parent;
 	}
+	new_parent->leaf = false;
 	left_node->parent = new_parent;
 	right_node->parent = new_parent;
-	free(parent);
-	//插入完成后，在看父节点是否满，向上递归处理
-	while(new_parent->degree == M){
-               split_node(root,new_parent,0);
-               if(new_parent->parent!=NULL){
-                       new_parent = new_parent->parent;
-               }else{
-                       //到跟节点了
-                       break;
-               }
-        }
-	free(node);
-	if(flag==1)
+	b_tree_node<T>* temp = new_parent;
+	while(temp!=NULL){
+		//递归
+		if(check_node_state(temp,4)){
+			split_node(root,temp,0);
+		}
+		temp = temp->parent;
+	}
+	if(flag==1){
+//		cout<<"a"<<endl;
 		return right_node;
-	else
+	}else if(flag==0){
 		return left_node;
+	}else{
+		return NULL;
+	}
 };
 //寻找插入位置
 template<typename T>
-b_tree_node<T>* b_tree<T>::find_insert_node(b_tree_node<T>*& root,b_tree_node<T>* cur,T x){
+b_tree_node<T>* b_tree<T>::find_insert_node(b_tree_node<T>**& root,b_tree_node<T>* cur,T x){
+//	cout<<"&root:"<<root<<endl;
 	//插入前，进行检查该节点的字节点是否满了
 	//b_tree_node<T>* cur = root;
-        int check_res = check_node_state(cur);
-        if(check_res){
-                //满了，进行分裂
-		if(x>cur->keys[cur->num/2]){
-			cur = split_node(root,cur,1);//右边
-		}else
-			///cout<<"d"<<endl;
-			cur = split_node(root,cur,0);//左边
-                if(cur==NULL){
-                        //分裂失败
-                        exit(0);
-                }
-        }
-	if(cur->leaf){
+//	cout<<"x:"<<x<<cur->num<<endl;
+//	cout<<"num"<<cur->num<<endl;
+//	if(cur->leaf==false){
+//		cout<<"false"<<endl;
+//	}else
+//		cout<<"true"<<endl;
+//	cout<<"key:"<<cur->keys[0]<<endl;
+	if(cur->leaf==true){
 		//在cur进行插入操作
+//		cout<<"x:"<<x<<endl;
+//		cout<<cur->num<<endl;
+//		cout<<cur->keys[0]<<endl;
+		int check_res = check_node_state(cur,3);
+//      	cout<<check_res<<endl;
+	        if(check_res){
+//              cout<<"aaaaaa"<<x<<endl;
+                //满了，进行分裂
+                	if(x>cur->keys[cur->num/2]){
+//                      	cout<<"11111111111111111111"<<endl;
+	//                      cout<<"x:"<<x;
+         	               	cur = split_node(root,cur,1);//右边
+//              	        cout<<cur->num<<endl;
+               		 }else
+                        	///cout<<"d"<<endl;
+                        	cur = split_node(root,cur,0);//左边
+               		 if(cur==NULL){
+                        	//分裂失败
+                        	exit(0);
+                	}
+        	}
+
 		return cur;
 	}
 	if(x<cur->keys[0]){
 		// 小于最小的，往左走
-		return find_insert_node(root,cur->childs[0],x);
-	}else if(x>cur->keys[cur->degree-2]){
+		if(cur->childs[0]==NULL)
+			return cur;
+		else
+			return find_insert_node(root,cur->childs[0],x);
+	}else if(x>cur->keys[cur->num-1]){
 		//大于最大的，往右走
 		//cout<<"ddd"<<endl;
-		return find_insert_node(root,cur->childs[cur->degree-1],x);
+		if(cur->childs[cur->num]==NULL){
+			return cur;
+		}else
+			return find_insert_node(root,cur->childs[cur->num],x);
 	}else{
 		//在这个节点上
-		for(int i = 1;i<cur->degree-1;i++){
+		cout<<"*******"<<endl;
+		for(int i = 1;i<cur->num;i++){
 			if(x>cur->keys[i])
 				continue;
 			if(x == cur->keys[i]){
@@ -251,8 +269,11 @@ b_tree_node<T>* b_tree<T>::find_insert_node(b_tree_node<T>*& root,b_tree_node<T>
 				exit(0);
 			}
 			if(x<cur->keys[i]){
-				//在i-1到i之间，接着去下一层的节点，该节点在字节点的坐标为i
-				return find_insert_node(root,cur->childs[i],x);
+	 			//在i-1到i之间，接着去下一层的节点，该节点在字节点的坐标为i
+				if(cur->childs[i]==NULL)
+					return cur;
+				else
+					return find_insert_node(root,cur->childs[i],x);
 			}
 		}	
 	}
@@ -267,73 +288,83 @@ b_tree_node<T>* b_tree<T>::find_node_by_key(T x){
 template<typename T>
 int b_tree<T>::insert_node(T x){
 	//寻找插入位置
+	b_tree_node<T>** root_temp = getRoot();
 	b_tree_node<T>* node = NULL;
-	if(getRoot()!=NULL){
+//	cout<<"&root_temp:"<<(root_temp)<<endl;
+	if(*root_temp!=NULL){
 		//树不为空
-		//cout<<"。"<<endl;
-		node = find_insert_node(root,root,x);
-		b_tree_node<T>* new_node = (b_tree_node<T>*)malloc(sizeof(b_tree_node<T>));
-		new_node->num = node->num+1;
-		new_node->degree = node->degree+1;
-		new_node->leaf = node->leaf;
-		new_node->parent = node->parent;
-		new_node->keys = (T*)malloc(sizeof(T)*new_node->num);
-		new_node->childs = (b_tree_node<T>**)malloc(sizeof(b_tree_node<T>*)*(new_node->degree));
-		int flag = true;
-		int insert_index = 0;
-		for(int i=0;i<node->num;i++){
-			if(node->keys[i]>x){
-				insert_index = i;
-				break;
-			}
-		}
-		for(int i=0;i<node->num;i++){
-			if(node->keys[i]!=0&&x>node->keys[i]){
-			//	cout<<"i:"<<i<<" "<<node->keys[i]<<endl;
-				new_node->keys[i] = node->keys[i];
-			}else{
-				if(flag){
-					new_node->keys[i] = x;
-					flag = false;
-					continue;
-				}
-				new_node->keys[i] = node->keys[i-1];
-			}
-			new_node->childs[i] = NULL;
-		}
-		if(insert_index==node->num){
-			new_node->keys[node->num] = x;
+		b_tree_node<T>* insert_place_node = find_insert_node(root_temp,*root_temp,x);
+//		cout<<insert_place_node->leaf<<endl;
+		//此时插入的节点一定在叶节点
+		b_tree_node<T>* new_node = new b_tree_node<T>(4,insert_place_node);
+		cout<<"****************************"<<endl;
+		print_tree(this->root);
+		cout<<"******************************"<<endl;
+		bool flag1 = true;
+                //处理keys
+                for(int i=0;i<insert_place_node->num;i++){
+//			cout<<"i:"<<i<<" key:"<<insert_place_node->keys[i]<<endl;
+                        if(insert_place_node->keys[i] < x){
+                                new_node->keys[i] = insert_place_node->keys[i];
+                                new_node->num++;
+                                continue;
+                        }
+                        if(flag1){
+				cout<<"i:"<<i<<x<<endl;
+                                new_node->keys[i] = x;
+  				new_node->num++;
+  				flag1 = false;
+                                continue;
+                        }
+                        new_node->keys[i] = insert_place_node->keys[i-1];
+                        new_node->num++;
+                }
+                if(flag1){
+                        //在最后进行添加
+                        new_node->keys[insert_place_node->num] = x;
+                        new_node->num++;
+                }else{
+//			cout<<"aaaaaa:wq"<<insert_place_node->keys[insert_place_node->num-1]<<endl;
+                        new_node->keys[insert_place_node->num] = insert_place_node->keys[insert_place_node->num-1];
+    			cout<<"aaaaaa:wq"<<insert_place_node->num<<endl;
+			new_node->num++;
+			cout<<new_node->num<<endl;
+                }
+                //处理childs
+                for(int i=0;i<new_node->num+1;i++){
+  			new_node->childs[i] = NULL;
+                }
+		new_node->parent = insert_place_node->parent;
+		if(insert_place_node->parent==NULL){
+//			cout<<"x:"<<x<<endl;
+			this->root = new_node;
 		}else{
-			new_node->keys[node->num] = node->keys[node->num-1];
-		}
-		new_node->childs[new_node->degree-2] = NULL;
-		new_node->childs[new_node->degree-1] = NULL;
-		if(node->parent!=NULL){
-			for(int i=0;i<node->parent->degree;i++){
-				if(node==node->parent->childs[i]){
-					node->parent->childs[i] = new_node;
-					//cout<<"dd"<<endl;
+			for(int i=0;i<new_node->parent->num+1;i++){
+				if(insert_place_node->parent->childs[i]==insert_place_node){
+					cout<<"77"<<endl;
+					new_node->parent->childs[i] = new_node;
+					cout<<new_node->num<<endl;
 					break;
 				}
 			}
-		}else{
-			root = new_node;
 		}
-		free(node);
-		node=NULL;
+//		cout<<"root:"<<getRoot()->keys[0]<<endl;
+		if(x==7){
+			//cout<<"root nun:"<<getRoot()->num<<endl;
+		}
+//		insert_place_node->degree++;
+		//插入之后再去判断是否存满
+//		if(check_node_state(new_node)){
+			//满了，分裂
+			//split_node(root,new_node,0);	
+//		}
 	}else{
 	//树为空
-		node = (b_tree_node<T>*)malloc(sizeof(b_tree_node<T>));
-		node->num = 1;
-		node->degree = 2;
-		node->leaf = true;
-		node->parent = NULL;
-		node->keys = (T*)malloc(sizeof(T)*node->num);
-		(node->childs) = (b_tree_node<T>**)malloc(sizeof(b_tree_node<T>*)*node->degree);
+//		cout<<"aaa"<<endl;
+		b_tree_node<T>* node = new b_tree_node<T>(4,true);
 		node->keys[0] = x;
-		node->childs[0] = NULL;
-		node->childs[1] = NULL;
-		root = node;
+		node->num++;
+		*root_temp = node;
 	}
 	return 1;
 };
@@ -343,21 +374,24 @@ template<typename T>
 int b_tree<T>::delete_node(T x){
 	return 0;
 };
+
 template<typename T>
 void b_tree<T>::print_tree(b_tree_node<T>* node){
-	if(node==NULL)
+	if(node==NULL){
+	//	cout<<"当前树为空"<<endl;
 		return;
-	int t = node->degree;
+	}
+	//int t = node->degree;
 	//cout<<"node_degree:"<<t<<endl;
-	for(int i=0;i<node->degree;i++){
+	for(int i=0;i<node->num+1;i++){
+//		cout<<"a"<<endl;
                 print_tree(node->childs[i]);
         }
-	cout<<"degree:"<<node->degree<<endl;
+	cout<<"num:"<<node->num<<endl;
 	for(int i=0;i<node->num;i++){
+	//	cout<<"qqq"<<node->num<<endl;
 		cout<<node->keys[i]<<" ";
 	}	
 	cout<<endl;
 }
-template<typename T>
-int b_tree<T>::M = 4;
 
--- btree_garbage/main.cpp
+++ btree_garbage/main.cpp
@@ -4,16 +4,17 @@ using namespace std;
 
 int main(){
 	b_tree<int>* tree = new b_tree<int>();
-	int arr[] = {4,3,1,51};
+	int arr[] = {100,3,20,50,70,60,80,90,110,120,115};
 //	tree->insert_node(5);
 	for(int i=0;i<sizeof(arr)/sizeof(int);i++){
 		tree->insert_node(arr[i]);
 	}
+//	cout<<"aaa"<<endl;
 //	tree->insert_node(4);
 //	tree->insert_node(3);
 //	tree->insert_node(1);
 //	tree->insert_node(2);
 //	tree->insert_node(5);
-	b_tree<int>::print_tree(tree->getRoot());
+	b_tree<int>::print_tree(*(tree->getRoot()));
 	return 0;
 }
